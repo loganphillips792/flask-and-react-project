6c8584ccdf1b020fe900dccae7fc21e3555ed30a
loganphillips792 <loganphillips792@gmail.com>
2023-08-30 22:07:10 -0400

    create backend folder

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -5,5 +5,5 @@ from app.models.models import User
 @app.route('/')
 def hello():
     app.logger.info('%s logged in successfully', 'logan564')
-    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
+    # users = db.session.execute(db.select(User).order_by(User.username)).scalars()
     return 'Hello, World!'
